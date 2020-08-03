bbcc6cfe775aa06e76fd5ac3acfbd657e024dd03
gleyconxavier <gleyconxcarlos@gmail.com>
2020-08-02 20:20:29 -0400

    Desafio concluído.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,39 @@
 import Transaction from '../models/Transaction';
+import e from 'express';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+    title: string;
+    type: "income" | "outcome";
+    value: number;
+}
+
+interface userData {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
+interface UserException {
+  message: string;
+  name: string;
+}
+
+enum Type {
+  INCOME = 'income',
+  OUTCOME = 'outcome',
+}
+
+function userException(message: string): UserException {
+  const name = "UserException";
+
+  const exception: UserException = ({ message, name });
+
+  return exception;
 }
 
 class TransactionsRepository {
@@ -13,16 +43,39 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): userData {
+    const balance = this.getBalance();
+    const transactions = this.transactions;
+    const data: userData = ({ transactions, balance });
+    return data;
+  }
+
+  private calculateBalance(operator: Type): number {
+    return this.transactions
+      .filter(t => t.type === operator)
+      .reduce((acc, tr) => acc + tr.value, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.calculateBalance(Type.INCOME);
+    const outcome = this.calculateBalance(Type.OUTCOME);
+    const total = income - outcome;
+
+      const balance: Balance = { income, outcome, total };
+      return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const { total } = this.getBalance();
+    if(type === "outcome" && total < value) {
+      throw new Error("Cannot create outcome transaction without a valid balance");
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
